bab2c371dd274f378b955a844def3f409b79d2e1
Hamzah-Aja <hamzahterryrizky@gmail.com>
2025-12-23 16:22:28 +0700

    change

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,42 +3,59 @@
 @section('title')
 <title>Absensi | Absens</title>
 @endsection
-@section('content')
 
-<div class="container mt-4" >
+@section('content')
+<div class="container mt-4">
     <div class="card">
         <div class="card-header">
-            Absen {{ $tgl->format('D d/m/Y')}}
+            Absen {{ $tgl->format('D d/m/Y') }}
         </div>
         <div class="card-body">
-        <form action="/absen/{{ $absen->id_absensi }}/update" method="POST">
-            @csrf
-                    <label for="siswa" class="mt-2 form-label"> Nama Siswa <span style="font-style: italic;">(required)</span></label>
-                        <select class="form-select" name="id_siswa" id="siswa">
-                        <option value="{{ $absen->id_siswa }}" selected> {{ $absen->siswa->nama }} </option>    
-                        @foreach ($siswa as $s)
-                            <option value="{{ $s->id_siswa }}">{{$s->nama}}</option>
-                            @endforeach
-                        </select>
+            <form action="/absen/{{ $absen->id_absensi }}/update" method="POST">
+                @csrf
 
-                        <label for="Ket" class="mt-2 form-label">Keterangan <span style="font-style: italic;">(required)</span></label>
-                        <select class="form-select" name="keterangan" id="Ket">
-                          <option value="{{ $absen->keterangan }}">{{ $absen->keterangan }}</option>
-                          <option value="Sakit">Sakit</option>
-                          <option value="Ijin">Ijin</option>
-                          <option value="Alfa">Alfa</option>
-                        </select>
-                        {{-- <label for="tanggal" class="mt-2 form-label">Tanggal <span style="font-style: italic;">(required)</span></label>
-                        <input type="date" class="mt-2 form-control" id="tanggal" name ='tanggal' required > --}}
+                {{-- Nama siswa --}}
+                <label for="siswa" class="mt-2 form-label">
+                    Nama Siswa <span style="font-style: italic;">(required)</span>
+                </label>
+                <select class="form-select" name="id_siswa" id="siswa">
+                    <option value="{{ $absen->id_siswa }}" selected>
+                        {{ optional($absen->siswa)->nama ?? '-' }}
+                    </option>
+                    @foreach ($siswa as $s)
+                        <option value="{{ $s->id_siswa }}">{{ $s->nama }}</option>
+                    @endforeach
+                </select>
 
-                        <label for="tanggal" class="mt-2 form-label">Tanggal <span style="font-style: italic;">(required)</span></label>
-                        <input type="text" value="{{ $tgl->isoFormat("ddd D-M-Y")}}" disabled class="mt-2 form-control"  id="tanggal">
+                {{-- Keterangan --}}
+                <label for="Ket" class="mt-2 form-label">
+                    Keterangan <span style="font-style: italic;">(required)</span>
+                </label>
+                <select class="form-select" name="keterangan" id="Ket">
+                    <option value="{{ $absen->keterangan }}">{{ $absen->keterangan }}</option>
+                    <option value="Hadir">Hadir</option>
+                    <option value="Sakit">Sakit</option>
+                    <option value="Ijin">Ijin</option>
+                    <option value="Alfa">Alfa</option>
+                </select>
 
-                        <input type="text" value="{{ $tgl }}" hidden class="mt-2 form-control" name ="tanggal" >
-                        <div class="mr-2">
-                        <button class="float-right btn btn-dark mt-4" type="submit">SUBMIT</button>
-                    </div>
-    </form>
+                {{-- Tanggal bisa dipilih --}}
+                <label for="tanggal" class="mt-2 form-label">
+                    Tanggal <span style="font-style: italic;">(required)</span>
+                </label>
+                <input
+                    type="date"
+                    class="mt-2 form-control"
+                    id="tanggal"
+                    name="tanggal"
+                    value="{{ \Carbon\Carbon::parse($absen->tanggal)->format('Y-m-d') }}"
+                    required
+                >
+
+                <div class="mr-2">
+                    <button class="float-right btn btn-dark mt-4" type="submit">SUBMIT</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
